021ef3ce8fe086140739692f0a6cb07cd0c248c7
Mario  Rodriguez <useraddmario@gmail.com>
2018-03-18 22:05:24 +0000

    Tests good!

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,20 @@
-
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
 
     def add_post(self, post):
-        pass
+        self.posts.append(post)
+        post.user = post.set_user(self)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for followed in self.following:
+            timeline += followed.posts
+        return sorted(timeline, key=lambda x: x.timestamp, reverse=False)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
